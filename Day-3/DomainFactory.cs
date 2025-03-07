e2ae9ac33de44b8f0d0f28c6ddc5336818ec1bfb
heathiest <118662440+metastases@users.noreply.github.com>
2025-03-07 13:42:53 +0530

    Add files via upload

---
+++ Day-3/DomainFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hamaraBasket.Tests
+{
+    public class DomainFactory
+    {
+        public DomainFactory()
+        {
+
+        }
+
+        public List<Item> CreateSingleItem(string name, int sellIn, int quality)
+        {
+            return new List<Item> { new Item { Name = name, SellIn = sellIn, Quality = quality } };
+        }
+
+        public void UpdateQuality(IList<Item> items)
+        {
+            HamaraBasket app = new HamaraBasket(items);
+            app.UpdateQuality();
+        }
+    }
+}
